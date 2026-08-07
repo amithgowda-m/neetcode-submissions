526bc287334126978a81c136fdbde8c90d37d4a2
Amith Gowda <amithgowdasmg@gmail.com>
2026-08-07 11:28:44 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) { //O(n+dlogd) : tc and O(d) sc
+    //d is number of distinct element in the nums array
+        unordered_map<int,int> mp;
+        for(int n: nums) mp[n]++; //we are storing frequency of each elememt here
+        vector<pair<int,int>> freq; //to store pair of freq and element
+        for(const auto&  p : mp) freq.push_back({p.second,p.first}); 
+        vector<int> ans;
+        sort(freq.rbegin(),freq.rend());// sorting based on frequency in descending order
+        for(int i=0;i<k && !freq.empty();i++) ans.push_back(freq[i].second); //getting max k frequent element
+        return ans;
+    }
+};
