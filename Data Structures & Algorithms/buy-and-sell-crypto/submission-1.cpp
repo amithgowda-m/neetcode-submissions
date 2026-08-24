2b823a9f50dad0cfcd8c59e152e46021ff127f8d
Amith Gowda <amithgowdasmg@gmail.com>
2026-08-24 11:50:08 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int bp = INT_MAX;
+        for(int i =0; i < prices.size()-1; i++){
+            if(prices[i]< prices[i+1]){
+                bp = min(bp,prices[i]);
+                
+            } 
+            else if(prices[i] > prices[i+1]){
+                ans = max(ans, prices[i] - bp);
+            } 
+            if(i == prices.size()-2 ){
+                if(prices[i+1]>prices[i]){
+                    ans = max(ans, prices[i+1] - bp);
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
