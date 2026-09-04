b2c555201e0423eb65615b1822718fb4e96ccf0b
Amith Gowda <amithgowdasmg@gmail.com>
2026-09-04 11:25:20 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+        vector<int> count(26);
+
+        int l = 0;
+        int r = s1.length()-1;
+        vector<int> window(26);
+        for(int i =0; i<s1.length(); i++) count[s1[i]-'a']++;
+        for(int i = l; i <=r; i++) window[s2[i]-'a']++;
+        while(r<s2.length()){
+            if(count == window) return true;
+            else{
+
+                window[s2[l]-'a']--;
+                l++;
+                r++;
+                if(r<s2.length())
+                    window[s2[r]-'a']++;
+            }
+        }
+        return false;
+    }
+};
